dfacd64b207ff659a43a8a44c5487bd28254cb77
mbabak <muskababak@ymail.com>
2020-06-15 17:28:27 -0400

    datatypes

---
+++ src/com/datatypes.java
@@ -0,0 +1,25 @@
+package com;
+
+public class datatypes {
+    public static void main(String[] args) {
+        boolean isSkyBlue = true;
+        // these three are not commonly used in our programs.
+        byte myByte = -128;
+        char myChar = 'a';
+        short myShortVariable = -30000;
+        int myInteger = 90000;
+        float myFloat = 7.42f;
+        double myDouble = -90.283333;
+        long LongVariable = 999999999;
+
+        System.out.println("My boolean variable :" + isSkyBlue);
+        System.out.println("My byte variable is equal to " + myByte);
+        System.out.println("My char variable is equal to " + myChar);
+        System.out.println("My short variable is equal to " + myShortVariable);
+        System.out.println("My integer plus 1k is equal to " + (myInteger + 1000) );
+
+
+
+
+    }
+}
